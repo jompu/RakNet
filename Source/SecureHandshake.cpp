02aed4f6e114479b790f4571fa9e1fddcf05eb56
Johannes Ylönen <johannes@indiumgames.fi>
2017-02-07 11:47:52 +0200

    A fix for Android. CAT_NO_ENTROPY_THREAD must be defined some where, so that pthread is disabled.

--- Source/SecureHandshake.cpp
+++ Source/SecureHandshake.cpp
@@ -3,7 +3,7 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
@@ -26,8 +26,13 @@
 #include "cat/src/port/EndianNeutral.cpp"
 #include "cat/src/port/AlignedAlloc.cpp"
 #include "cat/src/time/Clock.cpp"
+
 #include "cat/src/threads/Mutex.cpp"
+
+#if !defined(CAT_NO_ENTROPY_THREAD)
 #include "cat/src/threads/Thread.cpp"
+#endif
+
 #include "cat/src/threads/WaitableFlag.cpp"
 #include "cat/src/hash/MurmurHash2.cpp"
 #include "cat/src/lang/Strings.cpp"
